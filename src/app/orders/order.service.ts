9f54dbcda0a70c91079b4aafb9c8d17454857836
sarul <sarul.pat@northbkk.ac.th>
2020-10-02 16:02:19 +0700

    orders form

--- src/app/orders/order.service.ts
+++ src/app/orders/order.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -10,6 +10,25 @@ export class OrderService {
   constructor(private http: HttpClient) { }
 
   getOrder(): Observable<any[]> {
-    return this.http.get<any[]>('../../assets/order.json')
+    return this.http.get<any[]>('../../assets/orders.json')
+  }
+  getOrderById(id): Observable<any> {
+    if (id === 'new') {
+      return new Observable<any>(observable=>{
+        observable.next(
+          {
+            "id": id,
+            "date": "",
+            "name": "",
+            "status": "",
+            "orderTotal": 0,
+            "paymentMode": ""
+          }
+        )
+      })
+    } else {
+      return this.http.get<any[]>('../../assets/order.json')
+    }
+
   }
 }
